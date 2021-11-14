5876eba56de5f105c8ccc90dbffbac4ed237e94b
Patrick Faramaz <ipatate@users.noreply.github.com>
2021-11-14 11:18:57 +0100

    content system (#11)
    * add contentLayer
    * move static to public

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,37 @@
-import type { NextPage } from 'next'
+import { InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import { allBlogs } from '.contentlayer/data'
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import components from '../components/MDXComponents'
 
-const Home: NextPage = () => {
+const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [post] = posts
+  const Component = useMDXComponent(post.body.code)
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Head>
         <title>Goodmotion 2022</title>
       </Head>
       <main className="flex flex-col items-center justify-center mx-auto prose lg:prose-xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec hoc ille
-        non vidit, sed verborum magnificentia est et gloria delectatus. Iubet
-        igitur nos Pythius Apollo noscere nosmet ipsos. Itaque vides, quo modo
-        loquantur, nova verba fingunt, deserunt usitata. An dolor longissimus
-        quisque miserrimus, voluptatem non optabiliorem diuturnitas facit? Est
-        autem etiam actio quaedam corporis, quae motus et status naturae
-        congruentis tenet; Quod autem magnum dolorem brevem, longinquum levem
-        esse dicitis, id non intellego quale sit. Duo Reges: constructio
-        interrete. Ut id aliis narrare gestiant? Sed vos squalidius, illorum
-        vides quam niteat oratio. Hanc quoque iucunditatem, si vis, transfer in
-        animum;
+        <div className="relative w-full h-96">
+          <Image src={post.image} layout="fill" alt="Goodmotion" />
+        </div>
+        <h2>{post.title}</h2>
+        <div>{post.publishedAt}</div>
+        <Component components={components} />
       </main>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      posts: allBlogs,
+    },
+  }
 }
 
 export default Home
